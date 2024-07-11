1552043d72507c89d10c25285aaa7edeaff39fe5
Andreea1129 <vilcuandreea12@gmail.com>
2024-07-11 20:23:29 +0300

    hei

--- src/config/participants.ts
+++ src/config/participants.ts
@@ -4,4 +4,5 @@ export const participants = [
   { name: "Popirtac Dragos", github: "d0ghe" },
   { name: "Nitu Tiberiu-Florian", github: "Silentnvm" },
   { name: "Nazemian Darius", github: "dariusnp" },
+  { name: "Andreea Vilcu", github: "Andreea1129" }
 ];
