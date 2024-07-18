10956acc6f9f4c8bdc5d035ee2ae4108eed9d7aa
MDGcode <adrianboscanici@yahoo.com>
2024-07-18 19:01:42 +0300

    Adaugat nume

--- src/config/participants.ts
+++ src/config/participants.ts
@@ -1,5 +1,5 @@
 export const participants = [
-  { name: "Boscanici Adrian", github: "MDGcode" },
+  { name: "Boscanici Adrian Gabriel", github: "MDGcode" },
   { name: "Miloiu Cristi ", github: "cristim67" },
   { name: "Popirtac Dragos", github: "d0ghe" },
   { name: "Nitu Tiberiu-Florian", github: "Silentnvm" },
