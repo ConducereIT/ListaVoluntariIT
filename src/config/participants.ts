7b3a08e5d96b6540378b81d29f97bbb7c79ec80d
Cristi Miloiu <65810476+cristim67@users.noreply.github.com>
2024-07-18 18:40:04 +0300

    Update participants.ts

--- src/config/participants.ts
+++ src/config/participants.ts
@@ -1,6 +1,6 @@
 export const participants = [
   { name: "Boscanici Adrian", github: "MDGcode" },
-  { name: "Miloiu Cristi", github: "cristim67" },
+  { name: "Miloiu Cristi ", github: "cristim67" },
   { name: "Popirtac Dragos", github: "d0ghe" },
   { name: "Nitu Tiberiu-Florian", github: "Silentnvm" },
   { name: "Nazemian Darius", github: "dariusnp" },
